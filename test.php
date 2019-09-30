74525db4d7c94cc8804a8264a550914518e67b4a
ntakesawa <n.takesawa@varbarossa.jp>
2019-09-30 16:23:10 +0900

    retest

--- test.php
+++ test.php
@@ -7,24 +7,7 @@
 </head>
 
 <body>
-<?php
 
-$str = 'asdfasdfasdfsa/abcde/abcde-1.jpg';
-print $str;
-print '<br>';
-
-$str = str_replace('asdfasdfasdfsa/','',$str);
-print $str;
-print '<br>';
-$strat = strpos($str, '/');
-$rstrat = strrpos($str, '-');
-$front = substr($str, 0,$strat);
-print '全面:'.$front;
-$back = substr($str, $rstrat);
-print '後面:'.$back;
-print '<br>';
-
-?>
 
 </body>
 </html>
